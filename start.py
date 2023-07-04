af234793a86e281a327b0f60c044e1e2939967fa
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-07-04 08:58:31 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,40 @@
+import os
+
+from LegendSS.startup import *
+
+os.system("clear")
+
+
+vars = """
+APP_ID=
+API_HASH=
+BOT_TOKEN=
+"""
+
+opp = input(f"Want to fill vars ? if yes type Y/yes else press enter: ")
+if opp.lower() in ["y", "yes"]:
+    if not os.path.exists(".env"):
+        y = open(".env", "w")
+        y.write(vars)
+        y.close()
+        os.system("clear")
+        LegendStartUP()
+    elif os.path.exists(".env"):
+        f = open(".env")
+        check = f.read()
+        print(check)
+        f.close()
+        check_again()
+        if not len(lines) == 35:
+            os.system("rm -rf .env")
+            y = open(".env", "w")
+            y.write(vars)
+            y.close()
+            os.system("clear")
+            LegendStartUP()
+        else:
+            os.system("clear")
+            LegendStartUP()
+else:
+    os.system("clear")
+    os.system("python3 -m HackBot")
